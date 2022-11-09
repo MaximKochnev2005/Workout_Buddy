a3cc0e2d4d1f4f163d529497d8a6154f4eb0304d
m.kochnev <maksimkochnev563@gmail.com>
2022-11-09 22:33:11 +0300

    finall

--- Backend/controllers/workoutController.js
+++ Backend/controllers/workoutController.js
@@ -2,7 +2,9 @@ const Workout = require("../models/workoutModel")
 const mongoose = require("mongoose")
 // get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+
+    const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
 
     res.status(200).json(workouts)
 }
@@ -10,6 +12,8 @@ const getWorkouts = async (req, res) => {
 // get a single workout
 const getWorkout = async (req, res) => {
     const { id } = req.params
+    const user_id = req.user._id
+
     if(!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error: 'No such workout'})
     }
@@ -45,7 +49,8 @@ const createWorkout = async (req, res) => {
     }
 
     try {
-        const workout = await Workout.create({title, load, reps})
+        const user_id = req.user._id
+        const workout = await Workout.create({title, load, reps, user_id})
         res.status(200).json(workout)
     } catch (error){
         res.status(400).json({error: error.message})
